2988fe0412d35bc34c42a3d560e8bdf65cb949df
h-sumiya <sumiya.main01@gmail.com>
2023-10-09 01:04:21 +0900

    ai fix

--- ai/src/configs.rs
+++ ai/src/configs.rs
@@ -1,7 +1,7 @@
 pub const MUTATION_RATE: f32 = 0.01;
 pub const MUTATION_RANGE: f32 = 0.2;
 
-pub const DO_CROSS: usize = 150;
-pub const DO_MUTATION: usize = 600;
-pub const DO_CHANGE: usize = 100;
+pub const DO_CROSS: usize = 750;
+pub const DO_MUTATION: usize = 3000;
+pub const DO_CHANGE: usize = 500;
 pub const NUM_BOTS: usize = DO_CHANGE + DO_CROSS * 2 + DO_MUTATION;
